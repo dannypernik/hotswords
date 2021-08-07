71d02215be6b2595c8f1cecfa567b4e851e022a5
dannypernik <dannypernik@mail.com>
2021-08-07 12:10:03 -0500

    add order col to food table

--- app/models.py
+++ app/models.py
@@ -53,6 +53,7 @@ class Food(db.Model):
     price = db.Column(db.String(5))
     description = db.Column(db.String(1024))
     category = db.Column(db.String(64))
+    order = db.Column(db.Float())
 
     def __repr__(self):
         return '<Food {}>'.format(self.name)
