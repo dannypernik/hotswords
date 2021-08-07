617a590fd8d88bcb7abcc3f8c791b847ea587972
dannypernik <dannypernik@mail.com>
2021-08-07 15:08:48 -0500

    add veg to food db

--- app/models.py
+++ app/models.py
@@ -54,6 +54,7 @@ class Food(db.Model):
     description = db.Column(db.String(1024))
     category = db.Column(db.String(64))
     order = db.Column(db.Float())
+    veg = db.Column(db.Integer())
 
     def __repr__(self):
         return '<Food {}>'.format(self.name)
